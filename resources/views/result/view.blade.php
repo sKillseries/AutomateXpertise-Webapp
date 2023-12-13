f05cc14ba60a4408a4ae3960e84ad1d82063fa48
sKillseries <skillseries.infosec@gmail.com>
2023-12-13 13:58:36 +0100

    correction bug affichage

--- resources/views/result/view.blade.php
+++ resources/views/result/view.blade.php
@@ -1,6 +1,6 @@
 <x-app-layout>
     <x-slot name="header">
-        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+        <h2 class="font-semibold text-xl text-gray-800 leading-tight dark:text-white">
             {{ __("Contenu du fichier : $filename ") }}
         </h2>
     </x-slot>
